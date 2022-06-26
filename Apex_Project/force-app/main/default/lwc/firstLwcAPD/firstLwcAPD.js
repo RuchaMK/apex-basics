ff62801a2f1fd2cee0d20029df0425ecfae0d354
Ruchamk <ruchasf@gmail.com>
2022-06-26 21:14:09 +0530

    Imperative and wire in LWC

--- Apex_Project/force-app/main/default/lwc/firstLwcAPD/firstLwcAPD.js
+++ Apex_Project/force-app/main/default/lwc/firstLwcAPD/firstLwcAPD.js
@@ -1,14 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,wire } from 'lwc';
+import getAccounts from '@salesforce/apex/ApexLwcBasic.getAccounts';
+import getContacts from '@salesforce/apex/ApexLwcBasic.getContacts';
 
 export default class FirstLwcAPD extends LightningElement {
     msg='Please fill above details';
     inputCompanyName='';
+    @api msgData = 'Company informatiion';
+    result;
+    error;
 
     handlePreview(event){
         this.inputCompanyName = event.target.value;
-        window.console.log(event.target);
+        window.console.log('preview: ',event.target);
     }
     handleSave(event){
-        alert('**Saved**');
+        alert('**Display Contacts?**');
+        getContacts()
+          .then(result => {
+            this.result = result;
+            this.error = undefined;
+            console.log('Result', result);
+          })
+          .catch(error => {
+            this.result = undefined;
+            this.error = error;
+            console.error('Error:', error);
+        });
+
+       
     }
+
+    @wire(getAccounts) 
+    wiremethod({error,data}){
+            if(data){
+                this.result = data;
+                this.error = undefined;
+                window.console.log('**Data: ',data);
+            }else if(error){
+                this.error = error;
+                this.result = undefined;
+                window.console.log('**Error: ',error);
+            }
+        }
 }
