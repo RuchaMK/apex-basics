6210a8f762536f8e51a18cb3da4f31d650e56168
Ruchamk <ruchasf@gmail.com>
2022-07-04 23:05:42 +0530

    ignore

--- Apex_Project/force-app/main/default/aura/ParentChildApp/ParentChildApp.app
+++ Apex_Project/force-app/main/default/aura/ParentChildApp/ParentChildApp.app
@@ -1,3 +1,3 @@
 <aura:application extends="force:slds">
 <c:parentComponent/>
-</aura:application>	
+</aura:application>
--- Apex_Project/force-app/main/default/lwc/lds/lds.js
+++ Apex_Project/force-app/main/default/lwc/lds/lds.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class Lds extends LightningElement {}
+export default class Lds extends LightningElement {
+    hanldeError(){
+        alert('Error');
+    }
+    handleSuccess(){
+        alert('Success');
+    }
+    handleSubmit(){
+        alert('Submit');
+    }
+}
